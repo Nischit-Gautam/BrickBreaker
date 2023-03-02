b5a48328b02b13349108279f9886ac9cd3824652
Nischit <nischit.gautam@avishkaram.com>
2023-03-02 17:15:42 +0545

    removed neutonsoft

--- BrickBreaker/ReadGameFile.cs
+++ BrickBreaker/ReadGameFile.cs
@@ -1,9 +1,10 @@
-﻿using Newtonsoft.Json;
+﻿
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Reflection;
 using System.Text;
+using System.Text.Json;
 using System.Text.Json.Serialization;
 
 namespace BrickBreaker
@@ -16,15 +17,18 @@ namespace BrickBreaker
             string path = System.IO.Path.GetDirectoryName(Assembly.GetEntryAssembly().Location) + "\\gameInfo.txt";
             using (StreamReader reader = new StreamReader(path))
             {
-                var str = reader.ReadToEnd();
-                gameInfo = JsonConvert.DeserializeObject<GameInfo>(str);
+                string str = reader.ReadToEnd();
+                //gameInfo = JsonConvert.DeserializeObject<GameInfo>(str);
+                gameInfo = JsonSerializer.Deserialize<GameInfo>(str);
+
             }
             return gameInfo;
         }
         public void WriteSettingFile(GameInfo gameInfo)
         {
             string path = System.IO.Path.GetDirectoryName(Assembly.GetEntryAssembly().Location) + "\\gameInfo.txt";
-            string settingString = JsonConvert.SerializeObject(gameInfo);
+            //string settingString = JsonConvert.SerializeObject(gameInfo);
+            string settingString = JsonSerializer.Serialize(gameInfo);
             using (StreamWriter writer = new StreamWriter(path))
             {
                 writer.Write(settingString);               
